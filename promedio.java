e2ac1376839fe654d30ce4646e16e9fda9cb296b
adrian <ag4931249@gmail.com>
2024-10-05 16:30:20 -0500

    ejercicio promedio

---
+++ promedio.java
@@ -0,0 +1,13 @@
+public class promedio {
+    @SuppressWarnings("unused")
+    public static void main(String[] args) {
+        int [] numeros= {1,2,3,4,5};
+        System.out.println("la longitud es: " + numeros.length);
+        int acumulador=0;
+        for (int i=0; i<=numeros.length; i++ ){
+            
+        }
+    }
+
+
+}
